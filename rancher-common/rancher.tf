8b9cc6b2f90a76e580703428bc4d8c117d0a56d5
seanDeploy <63660232+seanDeploy@users.noreply.github.com>
2020-04-20 09:34:51 -0400

    rancher-common files

---
+++ rancher-common/rancher.tf
@@ -0,0 +1,41 @@
+# Rancher resources
+
+# Initialize Rancher server
+resource "rancher2_bootstrap" "admin" {
+  depends_on = [
+    helm_release.rancher_server
+  ]
+
+  provider = rancher2.bootstrap
+  
+  password  = var.admin_password
+  telemetry = true
+}
+
+# Create custom managed cluster for quickstart
+resource "rancher2_cluster" "quickstart_workload" {
+  provider = rancher2.admin
+
+  name        = var.workload_cluster_name
+  description = "Custom workload cluster created by Rancher quickstart"
+
+
+  rke_config {
+    kubernetes_version = var.workload_kubernetes_version
+    cloud_provider {
+      name = "aws"
+    }
+  }
+}
+
+# resource "rancher2_cluster_sync" "quickstart_workload" {
+#   cluster_id = rancher2_cluster.quickstart_workload.id
+# }
+
+# resource "rancher2_cluster_role_template_binding" "deploy" {
+#   count            = 1
+#   name             = "deploy"
+#   role_template_id = "cluster-owner"
+#   cluster_id       = rancher2_cluster_sync.quickstart_workload.id
+#   user_id          = var.rancher_deploy_user
+# }
